e9b73bc276fa9e7a57560cbc76c1606406d46067
Jasleenmultispecialityclinic <yarun9415@gmail.com>
2026-07-18 14:02:30 +0530

    Delete loader event listener and related code
    Removed loader functionality from the script.

--- script.js
+++ script.js
@@ -100,16 +100,3 @@ nav.classList.toggle("active");
 
 }
 
-// Loader
-
-window.addEventListener("load", function () {
-
-const loader = document.getElementById("loader");
-
-if(loader){
-
-loader.remove();
-
-}
-
-});
