5ae3556f18f02611c109f0f805c0d864b9de088b
Jasleenmultispecialityclinic <yarun9415@gmail.com>
2026-07-20 12:58:32 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -1,1 +1,14 @@
+const indiaBtn = document.querySelector(".india");
+const internationalBtn = document.querySelector(".international");
 
+if (indiaBtn) {
+    indiaBtn.addEventListener("click", function () {
+        localStorage.setItem("paymentDone", "India ₹300");
+    });
+}
+
+if (internationalBtn) {
+    internationalBtn.addEventListener("click", function () {
+        localStorage.setItem("paymentDone", "International ₹500");
+    });
+}
